a0609170eb487c7859f2e614bca9138f8e27c0f4
laughmun <thanat061143@gmail.com>
2024-06-12 18:27:26 +0700

    first commit HBD project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,86 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
 
 export default function Home() {
+  const [iHear, setIhear] = useState(true);
+  const [audioCont, setAudioCont] = useState(null);
+  const [analyz, setAnalyz] = useState(null);
+  // const [volumeNow, setVolume] = useState(0);
+  const audioRef = useRef(null);
+  let stream: any;
+
+  //ตรวจจับเสียง แปลงค่าเสียง เก็บค่าเสียง อัพเดทค่าเสียง
+  useEffect(() => {
+    if (audioCont && analyz) {
+      const dataAry = new Uint8Array(analyz.fftSize); //สร้างตัวแปรเก็บเสียง
+      const updateVolume = () => {
+        analyz.getByteTimeDomainData(dataAry); //ดึงค่าเสียงที่ได้เก็บในตัวแปรที่สร้าง ในรูปแบบ timedomain
+        const maxVolume = Math.max(...dataAry); //คำนวณหาค่าเสียงที่ดังที่สุด
+        // setVolume(maxVolume); //อัพเดทค่าความดังเสียง
+        if (maxVolume > 250) {
+          setIhear(false);
+        }
+        requestAnimationFrame(updateVolume);
+      };
+      updateVolume();
+    }
+  }, [audioCont, analyz]);
+
+  //เริ่มฟังเสียง
+  const startListen = async () => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.3;
+      audioRef.current.play();
+    }
+    if (!audioCont) {
+      const newAudioCont = new (window.AudioContext ||
+        window.webkitAudioContext)();
+      setAudioCont(newAudioCont);
+
+      stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      const source = newAudioCont.createMediaStreamSource(stream);
+      const newAnalyz = newAudioCont.createAnalyser();
+      newAnalyz.fftSize = 2048;
+      source.connect(newAnalyz);
+      setAnalyz(newAnalyz);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="">
+      <motion.div
+        className="w-full h-[20vh]"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, type: "spring" }}
+      >
+        <motion.div
+          className="mx-auto w-fit h-fit p-10 bg-white rounded-full mt-5 shadow-md hover:shadow-xl text-[60px]"
+          transition={{ duration: 0.5, type: "spring" }}
+          whileHover={{ scale: 1.1 }}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <button onClick={startListen} className=" text-[#F0C2C2] text-center">Happy Birth Day</button>
+          <audio ref={audioRef} src="/hbd-song.mp3" />
+        </motion.div>
+      </motion.div>
+      <motion.div
+        className="relative w-fit h-[] flex items-end justify-center mx-auto  "
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.5, type: "spring" }}
+      >
+        <Image src="/cakewithcandle.png" alt="cake" priority width={580} height={500} />
+        {iHear ? (
+          <motion.div 
+            animate={{opacity: [1 , 0.9 , 1] , height: [56 , 58 , 56] , scale: [1 , 1.1 , 1]}}
+            transition={{duration: 1 , repeat: Infinity, repeatDelay: 1 , times: [0 , 0.2 , 0.5 ,0.8 , 1]}}
+            className=" absolute bg-[#ffffff] h-14 w-12 top-9 rounded-full border-[13px] border-[#f1c777] blur-sm" />
+          ) : (
+          <div/>
+        )}
+      </motion.div>
     </main>
   );
 }
